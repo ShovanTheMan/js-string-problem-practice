ce6938b6046d7e57316c814a4247c32348402578
ShovanTheMan <shovan2192@gmail.com>
2023-10-05 02:16:44 +0600

    comparision added

---
+++ max3.js
@@ -0,0 +1,16 @@
+const jim = 84;
+const dela = 90;
+const cinku = 97;
+
+if(jim > dela && jim > cinku){
+    console.log('Jim will get the cake');
+}
+else if(dela > jim && dela > cinku){
+    console.log('Dela will get the cake');
+}
+else{
+    console.log('Cinku will get the cake');
+}
+
+// Math.max(34, 56, 78, 89)
+// Math.min(34, 56, 78, 89)
